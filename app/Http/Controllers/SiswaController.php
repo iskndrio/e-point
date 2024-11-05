efa37dd0d9cfd0b5ec2da79e4d293bc48c0d093b
Rafi <rafi.haykal2007@smkn4-tsm.sch.id>
2024-11-05 15:25:23 +0700

    aplikasi e-point

---
+++ app/Http/Controllers/SiswaController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Siswa;
+use App\Models\USer;
+use Illuminate\Http\Response;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class SiswaController extends Controller
+{
+    public function index(): view {
+        //get data db
+        $siswas = DB::table('siswas')
+        ->join('users', 'siswas.id_user', '=', 'users.id')
+        ->select(
+            'siswas.*',
+            'users.name',
+            'users.email'
+        )
+        ->paginate(10);
+
+        return view('admin.siswa.index', compact('siswas'));
+    }
+}
